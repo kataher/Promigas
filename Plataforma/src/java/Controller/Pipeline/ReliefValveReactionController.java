9bad0c8cd69e5f52ccabbb56bf88eda8f8f4f0db
kataher <karena@uninorte.edu.co>
2019-07-21 15:09:32 -0500

    CRUD

--- Plataforma/src/java/Controller/Pipeline/ReliefValveReactionController.java
+++ Plataforma/src/java/Controller/Pipeline/ReliefValveReactionController.java
@@ -28,82 +28,25 @@ public class ReliefValveReactionController extends Controller.Controller {
     
     public  Map<String, String> getValues(HttpServletRequest request) throws Exception{
          
-        String description_rvr = request.getParameter("description_rvr");
-        String opsk_rvr = request.getParameter("opsk_rvr");
-        
-        String molecularw_rvr = request.getParameter("molecularw_rvr");
-        String specifich_rvr = request.getParameter("specifich_rvr");
-        String criticalf_rvr = request.getParameter("criticalf_rvr");
-        String specificg_rvr = request.getParameter("specificg_rvr");
-        
-        String enteree_rvr = request.getParameter("enteree_rvr");
-        String requiredf_rvr = request.getParameter("requiredf_rvr");
-        String temperatureo_rvr = request.getParameter("temperatureo_rvr");
-        String staticp_rvr = request.getParameter("staticp_rvr");
-        String areao_rvr = request.getParameter("areao_rvr");
-        
-        String pesomolecular_rvr = request.getParameter("pesomolecular_rvr");
-        String specificgra_rvr = request.getParameter("specificgra_rvr");
-        String specifichs_rvr = request.getParameter("specifichs_rvr");
-        String criticalfw_rvr = request.getParameter("criticalfw_rvr");
-
-        String selectgv_sel_rvr = request.getParameter("selectgv_sel_rvr");
-        String rf_sel_rvr = request.getParameter("rf_sel_rvr");
-        String tempo_sel_rvr = request.getParameter("tempo_sel_rvr");
-        String sp_sel_rvr = request.getParameter("sp_sel_rvr");
-        String ao_sel_rvr = request.getParameter("ao_sel_rvr");
-        String ee_sel_rvr = request.getParameter("ee_sel_rvr");
-
-        String reactionf_rvr = request.getParameter("reactionf_rvr");
-        
-        //ids
-        String id_proyect = request.getParameter("idproyect");
-        String iduser = request.getParameter("iduser");
+        Map<String, String[]> vals = request.getParameterMap();
         
         Map<String, String> values = new HashMap<String, String>();
-                
-        try
-        {
-            values.put("opsk_rvr", stringToBD(opsk_rvr.trim()));
-            values.put("description_rvr", stringToBD(description_rvr.trim()));
+        
+        for (String key : vals.keySet()){
             
-            values.put("molecularw_rvr", stringToBD(molecularw_rvr.trim()));
-            values.put("specifich_rvr", stringToBD(specifich_rvr.trim()));
-            values.put("criticalf_rvr", stringToBD(criticalf_rvr.trim()));
-            
-            values.put("requiredf_rvr", stringToBD(requiredf_rvr));
-            values.put("temperatureo_rvr", stringToBD(temperatureo_rvr.trim()));
-            values.put("staticp_rvr", stringToBD(staticp_rvr.trim()));
-            values.put("areao_rvr", stringToBD(areao_rvr.trim()));
-            
-            values.put("specifichs_rvr", stringToBD(specifichs_rvr));
-            values.put("criticalfw_rvr", stringToBD(criticalfw_rvr.trim()));
-            values.put("specificg_rvr", stringToBD(specificg_rvr.trim()));
-            values.put("enteree_rvr", stringToBD(enteree_rvr.trim()));
-            values.put("pesomolecular_rvr", stringToBD(pesomolecular_rvr.trim()));
-            values.put("specificgra_rvr", stringToBD(specificgra_rvr.trim()));
-            values.put("ee_sel_rvr", stringToBD(ee_sel_rvr.trim()));
-            
-            values.put("selectgv_sel_rvr", stringToBD(selectgv_sel_rvr.trim()));
-            values.put("tempo_sel_rvr", stringToBD(tempo_sel_rvr));
-            values.put("sp_sel_rvr", stringToBD(sp_sel_rvr));
-            values.put("ao_sel_rvr", stringToBD(ao_sel_rvr));
-            values.put("rf_sel_rvr", stringToBD(rf_sel_rvr));
-            
-
-            //Results
-            values.put("reactionf_rvr", stringToBD(reactionf_rvr.trim()));
-            
-            values.put("id_proyect", id_proyect.trim());
-            values.put("id_user", iduser.trim());
-            
-       
-            
-        }catch(Exception ex){
-            throw new Exception(ex.getMessage());
+            if(!key.equals("opcion") && !key.equals("from") && !key.equals("opt_" + vals.get("from")[0])) {
+                if(key.equals("id_" + vals.get("from")[0]))
+                    values.put("id", stringToBD(vals.get(key)[0].trim()));
+                else if (key.equals("proyects_sel_" + vals.get("from")[0]))
+                    values.put("id_proyect", stringToBD(vals.get(key)[0].trim()));
+                else
+                    values.put(key, stringToBD(vals.get(key)[0].trim()));
+            }
         }
-       
-       return values;
+        
+        
+        
+        return values;
         
     }
        
--- Plataforma/src/java/Controller/Pipeline/ReliefValveSizingController.java
+++ Plataforma/src/java/Controller/Pipeline/ReliefValveSizingController.java
@@ -24,86 +24,25 @@ public class ReliefValveSizingController extends Controller.Controller {
     
     public  Map<String, String> getValues(HttpServletRequest request) throws Exception{
          
-        String description_rvs = request.getParameter("description_rvs");
-        String opso_rvs = request.getParameter("opso_rvs");
-        String opsk_rvs = request.getParameter("opsk_rvs");
-        String specifich_rvs = request.getParameter("specifich_rvs");
-        String criticalf_rvs = request.getParameter("criticalf_rvs");
-        String relief_rvs = request.getParameter("relief_rvs");
-        String gasvaporf_rvs = request.getParameter("gasvaporf_rvs");
-        String gasp_rvs = request.getParameter("gasp_rvs");
-        String requiredf_rvs = request.getParameter("requiredf_rvs");
-        String requiredeff_rvs = request.getParameter("requiredeff_rvs");
-        String effectivec_rvs = request.getParameter("effectivec_rvs");
-        String capacityc_rvs = request.getParameter("capacityc_rvs");
-        String compressibilityf_rvs = request.getParameter("compressibilityf_rvs");
-        String molecularws_rvs = request.getParameter("molecularws_rvs");
-        String specificg_rvs = request.getParameter("specificg_rvs");
-
-        String selectgv_sel_rvs = request.getParameter("selectgv_sel_rvs");
-        String rv_sel_rvs = request.getParameter("rv_sel_rvs");
-        String gfv_sel_rvs = request.getParameter("gfv_sel_rvs");
-        String bp_sel_rvs = request.getParameter("bp_sel_rvs");
-        String rf_sel_rvs = request.getParameter("rf_sel_rvs");
-        String cc_sel_rvs = request.getParameter("cc_sel_rvs");
-        String re_sel_rvs = request.getParameter("re_sel_rvs");
-
-        String criticafr_rvs = request.getParameter("criticafr_rvs");
-        String flowingc_rvs = request.getParameter("flowingc_rvs");
-        String requirede_rvs = request.getParameter("requirede_rvs");
-        String requiredfo_rvs = request.getParameter("requiredfo_rvs");
-        String ccoefficient_rvs = request.getParameter("ccoefficient_rvs");
-        
-        //ids
-        String id_proyect = request.getParameter("idproyect");
-        String iduser = request.getParameter("iduser");
+       Map<String, String[]> vals = request.getParameterMap();
         
         Map<String, String> values = new HashMap<String, String>();
-                
-        try
-        {
-            values.put("description_rvs", stringToBD(description_rvs.trim()));
-            values.put("specifich_rvs", stringToBD(specifich_rvs.trim()));
-            values.put("criticalf_rvs", stringToBD(criticalf_rvs.trim()));
-            values.put("compressibilityf_rvs", stringToBD(compressibilityf_rvs.trim()));
-            values.put("molecularws_rvs", stringToBD(molecularws_rvs.trim()));
-            values.put("specificg_rvs", stringToBD(specificg_rvs.trim()));
-            values.put("opso_rvs", stringToBD(opso_rvs.trim()));
-            values.put("opsk_rvs", stringToBD(opsk_rvs.trim()));
-            values.put("relief_rvs", stringToBD(relief_rvs));
-            values.put("gasvaporf_rvs", stringToBD(gasvaporf_rvs));
-            values.put("gasp_rvs", stringToBD(gasp_rvs));
-            values.put("requiredf_rvs", stringToBD(requiredf_rvs));
-            values.put("requiredeff_rvs", stringToBD(requiredeff_rvs.trim()));
-            values.put("effectivec_rvs", stringToBD(effectivec_rvs));            
-            values.put("capacityc_rvs", stringToBD(capacityc_rvs));
+        
+        for (String key : vals.keySet()){
             
-            values.put("selectgv_sel_rvs", stringToBD(selectgv_sel_rvs.trim()));
-            values.put("rv_sel_rvs", stringToBD(rv_sel_rvs));
-            values.put("gfv_sel_rvs", stringToBD(gfv_sel_rvs));
-            values.put("bp_sel_rvs", stringToBD(bp_sel_rvs));
-            values.put("rf_sel_rvs", stringToBD(rf_sel_rvs));
-            values.put("cc_sel_rvs", stringToBD(cc_sel_rvs));
-            values.put("re_sel_rvs", stringToBD(re_sel_rvs));
-            
-
-            //Results
-            values.put("criticafr_rvs", stringToBD(criticafr_rvs.trim()));
-            values.put("flowingc_rvs", stringToBD(flowingc_rvs.trim()));
-            values.put("requirede_rvs", stringToBD(requirede_rvs.trim()));
-            values.put("requiredfo_rvs", stringToBD(requiredfo_rvs.trim()));
-            values.put("ccoefficient_rvs", stringToBD(ccoefficient_rvs.trim()));
-            
-            values.put("id_proyect", id_proyect.trim());
-            values.put("id_user", iduser.trim());
-            
-       
-            
-        }catch(Exception ex){
-            throw new Exception(ex.getMessage());
+            if(!key.equals("opcion") && !key.equals("from") && !key.equals("opt_" + vals.get("from")[0])) {
+                if(key.equals("id_" + vals.get("from")[0]))
+                    values.put("id", stringToBD(vals.get(key)[0].trim()));
+                else if (key.equals("proyects_sel_" + vals.get("from")[0]))
+                    values.put("id_proyect", stringToBD(vals.get(key)[0].trim()));
+                else
+                    values.put(key, stringToBD(vals.get(key)[0].trim()));
+            }
         }
-       
-       return values;
+        
+        
+        
+        return values;
         
     }
     
